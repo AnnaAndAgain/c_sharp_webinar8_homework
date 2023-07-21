7b64ccb595b777086303433cc3df9da077370d0a
Anna <a.s.markovskaya@gmail.com>
2023-07-21 11:21:38 +0300

    задачка со спиралью сдалась!

--- task-62/Program.cs
+++ task-62/Program.cs
@@ -1,7 +1,4 @@
-﻿// Эта задача мне не далась.
-
-
-/*
+﻿/*
 Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 Например, на выходе получается вот такой массив:
 01 02 03 04
@@ -27,42 +24,45 @@ void PrintArray(int[,] tmpArray)
 void Spiral(int[,] tmpArray)
 {
     int count = 1;
-    int maxI = tmpArray.GetLength(0);
-    int maxJ = tmpArray.GetLength(1);
+    int maxCount = tmpArray.GetLength(0)*tmpArray.GetLength(1);
+    int maxI = tmpArray.GetLength(0)-1;
+    int maxJ = tmpArray.GetLength(1)-1;
     int minI = 0;
     int minJ = 0;
-
-    while (count < 15)
+    while (count <= maxCount)
     {
-        maxJ = maxJ - 1;
         for (int i = minI; i <= maxI; i++)
         {
             tmpArray[i, minJ] = count;
             count++;
+            if (count > maxCount) return;
         }
-        
-        for (int j = 1; j <= maxJ; j++)
+        minJ++;
+
+        for (int j = minJ; j <= maxJ; j++)
         {
             tmpArray[maxI, j] = count;
             count++;
+            if (count > maxCount) return;
         }
-        maxI = maxI - 1;
+        maxI--;
 
         for (int i = maxI; i >= minI; i--)
         {
             tmpArray[i, maxJ] = count;
             count++;
+            if (count > maxCount) return;
         }
-        minJ = minJ +1;
+        maxJ--;
 
         for (int j = maxJ; j >= minJ; j--)
         {
             tmpArray[minI, j] = count;
             count++;
+            if (count > maxCount) return;
         }
         minI++;
-        
-    }
+    }   
 }
 
 
